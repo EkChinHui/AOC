9edb90eb6d86ab50a3427de280656030c33b7e7b
EkChinHui <57344608+EkChinHui@users.noreply.github.com>
2020-12-09 22:55:44 +0800

    Add Day 6 solutions part 2

--- 6/form_checker.py
+++ 6/form_checker.py
@@ -24,11 +24,32 @@ def sum_of_unique_answers(data):
         counter += unique_answers_in_group(group) 
     return counter
 
+def common_answers_in_group(group):
+    common_answer_set = group[0]
+    for person in group:
+        common_answer_set = [value for value in person if value in common_answer_set]
+    return len(common_answer_set)
+
+def sum_of_common_answers(data):
+    counter = 0
+    for group in data:
+        counter += common_answers_in_group(group) 
+    return counter
+
 def main():
     forms_data = open("input.txt", "r")
     groups_of_forms = process_forms_input(forms_data.read())
     part_one = sum_of_unique_answers(groups_of_forms)
     print("Part 1: {}".format(part_one))
+
+    part_two = sum_of_common_answers(groups_of_forms)
+    print("Part 2: {}".format(part_two))
+
+'''
+Part 2 Intuition:
+Use sets as well and use the intersection between sets
+'''
+
 
 if __name__ == "__main__":
     main()
